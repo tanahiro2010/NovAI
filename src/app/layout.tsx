4231529f9c05a53b566c4720a68ffab78a87b52a
tanahiro2010 <herentongkegu087@gmail.com>
2025-03-23 10:50:44 +0900

    delete data export from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,29 +11,19 @@ import { Toaster } from "@/components/ui/sonner";
 
 const MochiyPopPOne = Mochiy_Pop_P_One({ weight: "400", subsets: ["latin"] });
 
-interface data {
-  isLogin: null | Session;
-  data: any;
-}
-
 export const metadata: Metadata = {
   title: "NovAI / 小説執筆をAIがサポートしてくれるサービス",
   description: "NovAIは、AIがあなたの小説執筆をサポートする革新的なサービスです。プロットの構築、文章の推敲、キャラクター設定まで、創作のすべての段階でAIがあなたのパートナーとなります。",
 };
 
-export const data: data = {
-  isLogin: null,
-  data: {}
-}
-
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  data.isLogin = await isLogin();
+  const login: null | Session = await isLogin();
 
   return (
     <html lang="ja">
       <body className={`bg-[#FAEBD7] ${MochiyPopPOne.className} flex flex-col`}>
         <div className="fixed top-0 left-0 w-full z-50">
-          <Header />
+          <Header login={login} />
         </div>
         <main className="flex-grow mt-20 bg-[#FAEBD7]">
           {children}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,12 @@
-import { data } from "@/app/layout";
 import Link from "next/link";
 import A from "./ui/link";
+import { Session } from "@supabase/supabase-js";
 
-export default async function Header() {
+interface Props {
+    login: Session | null;
+}
+
+export default async function Header({ login }: Props) {
     return (
         <header className="p-5 sm:p-5 flex justify-between items-center bg-gradient-to-b from-[#FAEBD7] via-[#FAEBD7] to-background">
             <div className="text-xl sm:text-3xl text-black">
@@ -14,8 +18,8 @@ export default async function Header() {
             </div>
 
             <div className="text-sm sm:text-base">
-                <A href={ data.isLogin ? "/dashboard" : "/login" } >
-                    { data.isLogin ? "ダッシュボード" : "ログイン" }
+                <A href={ login ? "/dashboard" : "/login" } >
+                    { login ? "ダッシュボード" : "ログイン" }
                 </A>
             </div>
         </header>
