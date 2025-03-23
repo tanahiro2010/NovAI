f579448abefad4c386caa7ad0880f8688f0392ea
tanahiro2010 <herentongkegu087@gmail.com>
2025-03-24 05:36:40 +0900

    edit dashboard. add control section

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ export default async function Dashboard() {
     return (
         <div className="container mx-auto px-4 py-4 max-w-screen-xl">
             {/* ヘッダーセクション */}
-            <div className="mb-6 sm:mb-8">
+            <div className="mb-3 sm:mb-8">
                 <h1 className="text-2xl sm:text-3xl font-bold text-gray-900 mb-2">マイダッシュボード</h1>
                 <p className="text-sm sm:text-base text-gray-600">あなたの創作活動をサポートします</p>
             </div>
@@ -19,7 +19,7 @@ export default async function Dashboard() {
                     <div className="p-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
                         <OptionLink
                             href="/dashboard/create/novel"
-                            title="作品作成"
+                            title="小説作成"
                             description="新しい小説を書き始めましょう"
                         />
                         <OptionLink
@@ -31,6 +31,30 @@ export default async function Dashboard() {
                             href="/dashboard/create/char"
                             title="キャラクター作成"
                             description="登場人物を設定しましょう"
+                        />
+                    </div>
+                </fieldset>
+
+                <fieldset className="mt-4 sm:mt-5 border rounded-md p-2 sm:p-4">
+                    <legend className="text-xl sm:text-2xl text-gray-900 mb-2 ml-[3%] px-1">
+                        作品管理
+                    </legend>
+
+                    <div className="p-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
+                        <OptionLink
+                            href="/dashboard/create/novel"
+                            title="小説管理"
+                            description="自身の小説を管理しましょう"
+                        />
+                        <OptionLink
+                            href="/dashboard/create/plot"
+                            title="プロット管理"
+                            description="過去のプロットデータを見直しましょう"
+                        />
+                        <OptionLink
+                            href="/dashboard/create/char"
+                            title="キャラクター管理"
+                            description="過去の登場人物データを見直しましょう"
                         />
                     </div>
                 </fieldset>
